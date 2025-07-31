f90b993a633d7a2c6f353423c0e844bdd3b39fb0
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-07-31 23:39:59 +0530

    BASICS

--- 02. ARRAYS/01. ARRAY QUESTIONS/05. FIND AN ELEMENT IN AN ARRAY.java
+++ 02. ARRAYS/01. ARRAY QUESTIONS/05. FIND AN ELEMENT IN AN ARRAY.java
@@ -2,29 +2,32 @@
 // Then take n integer inputs and store them in the array one by one
 // Your given an another number d
 // Your required to check if d exists in the array a and at what index ( 0 based ) If found print the index other wise print -1
+
 import java.util.Scanner;
 public class array{
     public static void main(String[] args){
         Scanner input= new Scanner(System.in);
         int n= input.nextInt();
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++){
-             arr[i]=input.nextInt();             
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = input.nextInt();             
         }
         System.out.println("Your target is");
-        Scanner inputs= new Scanner(System.in);
-        int target=inputs.nextInt();
-        for(int i=0;i<n;i++){
-              if(arr[i]==target){
-                System.out.println("Your target index is:"+i);
-              }
-              else{
-                System.out.println("-1");
+        int target = input.nextInt();
+        boolean found = false;
+        for(int i=0; i<n; i++){
+            if(arr[i] == target){
+                System.out.println("Your target index is:" + i);
+                found = true;
                 break;
-              }
+            }
+        }
+        if(!found){
+            System.out.println("-1");
         }
     }
 }
+
 // INPUT  = 5 
 //        = 1
 //        = 2
