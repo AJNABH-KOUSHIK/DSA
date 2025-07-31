a3d3c8dbc177fad2d74be46b44a797be8cdb38c1
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-07-31 20:06:30 +0530

    BASICS

--- 02. ARRAYS/01. ARRAY QUESTIONS/02. PRINT ARRAY ELEMENTS REVERSE LINEWISE.java
+++ 02. ARRAYS/01. ARRAY QUESTIONS/02. PRINT ARRAY ELEMENTS REVERSE LINEWISE.java
@@ -1,3 +1,7 @@
+// Print Array Elements Reverse Linewise
+// Take n as an integer input. Declare an array of size n that stores value of int data-type.
+// Then take n integer inputs and store them in the array one by one. 
+// Then print the elements of the array from the last index till the 0th index such that each element is printed one by one in each line.
 import java.util.Scanner;
 public class Reverse{
     public static void main(String[] args){
