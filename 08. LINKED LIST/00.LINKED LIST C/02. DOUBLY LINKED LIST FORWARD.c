8f13e9e7450008a3e3d51df29b38267fca1bfb1a
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-08-16 16:40:35 +0530

    BASICS

---
+++ 08. LINKED LIST/00.LINKED LIST C/02. DOUBLY LINKED LIST FORWARD.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node { // same for both singly and doubly
+    int data; // same for both singly and doubly
+    struct Node *next, *prev; // same for both singly and doubly
+}; // same for both singly and doubly
+int main() { // same for both singly and doubly
+    struct Node *head=NULL; // same for both singly and doubly
+    struct Node *temp=NULL; // same for both singly and doubly
+    struct Node *newNode=NULL; // same for both singly and doubly
+    int n,val;
+    printf("How many nodes? ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        printf("Enter value: ");
+        scanf("%d", &val);
+        newNode = (struct Node*)malloc(sizeof(struct Node)); 
+        newNode->data = val; 
+        newNode->next = NULL;
+        newNode->prev = NULL;
+        if (!head) {
+            head = newNode;
+        } else {
+            temp = head;
+            while (temp->next)
+            temp = temp->next;
+            temp->next = newNode;
+            newNode->prev = temp;
+        }
+    }
+    printf("Forward: ");
+    temp = head;
+    while (temp!=NULL) {  // same for both singly and doubly
+        printf("%d ", temp->data); // same for both single and doubly
+        temp = temp->next; // same for both singly and doubly
+    } // same for both singly and doubly
+    printf("null"); // same for both singly and doubly
+    return 0; // same for both singly and doubly
+} // same for both singly and doubly
