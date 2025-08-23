988c3ed5a662d59c4fdde671c7d2a854d0eb81e6
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-08-23 10:39:51 +0530

    BASICS

---
+++ 03. SEARCHING/00. SEARCHING IN C/01.LINEAR SEARCH/03. MAX ELEMENT IN AN ARRAY.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int ans = arr[0]; // assume first element is min
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > ans) {
+            ans = arr[i];
+        }
+    }
+    printf("Maximum element = %d\n", ans);
+    return 0;
+}
