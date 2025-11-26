11301b4165a3324d4d6b59a6a53caeb88344223f
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-11-26 17:32:37 +0530

    Update 03. IMPLEMENTATION STACKS USING ARRAYS.c

--- 09. STACKS/00. STACKS IN C/01. STACKS QUESTIONS /03. IMPLEMENTATION STACKS USING ARRAYS.c
+++ 09. STACKS/00. STACKS IN C/01. STACKS QUESTIONS /03. IMPLEMENTATION STACKS USING ARRAYS.c
@@ -1,54 +1,30 @@
 #include <stdio.h>
-#define limit 5
-int top=-1;
-int stack[limit];
-void push(int value){
-    if((top+1)==limit){
-        printf("Stack overflow");
-        return;
-    }
-    top++;
-    stack[top]=value;
+#define SIZE 5
+int stack[SIZE];
+int top = -1;
+void push(int x) {
+    if (top == SIZE - 1)
+        printf("Overflow\n");
+    else
+        stack[++top] = x;
 }
-void display(){
-    if(top==-1){ 
-        printf("Stack is empty");
-        return;
-    }
-    printf("The stack elements are:");
-    for(int i=0;i<=top;i++){
-        printf("%d ",stack[i]);
-    }
+int pop() {
+    if (top == -1)
+        return -1;   // Underflow
+    return stack[top--];
+}
+void display() {
+    for (int i = top; i >= 0; i--)
+        printf("%d ", stack[i]);
+    printf("\n");
 }
 int main() {
-    printf("Enter the value to push(press x to exit):");
-    int value;
-    scanf("%d",&value);
-    push(value);
-    scanf("%d",&value);
-    push(value);
-    display();
+    push(10);
+    push(20);
+    push(30);
+    display();  // 30 20 10
+    printf("Popped: %d\n", pop());  // 30
+    printf("Popped: %d\n", pop());  // 20
+    display(); // 10
     return 0;
-//    ( OR )
-//     printf("Enter the value to push(press x to exit):");
-//     int value;
-//     char choice;
-//     while (1) {
-//         printf("\nEnter your choice: ");
-//         scanf(" %c", &choice); 
-//          if (choice == 'x') {
-//             printf("Exiting program.\n");
-//             break;
-//         } else if (choice == 'p') {
-//             printf("Enter the value to push: ");
-//             scanf("%d", &value);
-//             push(value);
-//             display();
-//         }  else {
-//             printf("Invalid choice. Please try again.\n");
-// }
-//     }
-//         pop();
-//  display();
-//     return 0;
 }
